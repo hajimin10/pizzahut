4901f0db2fa84eb4e653a9dc8966e5f6264e19ee
hajimin <hajimin10@naver.com>
2025-01-24 14:07:26 +0900

    모바일 디자인 완성

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const newslSlider = new Swiper(".news-slider", {
 
     // autoplay: true,
     speed: 1000,
-    slidesPerView: 4, //한 번에 보여줄 슬라이드 갯수
+    slidesPerView: 2, //한 번에 보여줄 슬라이드 갯수
     spaceBetween: 20,
 
     // If we need pagination
@@ -19,4 +19,36 @@ const newslSlider = new Swiper(".news-slider", {
         nextEl: ".news-slider-wrap .btn-next",
         prevEl: ".news-slider-wrap .btn-prev",
     },
+
+    /* 반응형 */
+    breakpoints: {
+        /* 768px 이상일 때 슬라이드 갯수 3개 */
+        768: {
+            slidesPerView: 4,
+        },
+    },
+});
+
+// 버튼 요소 가져오기
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// 스크롤 이벤트 감지
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        // 스크롤이 300px 이상 내려가면 버튼 표시
+        scrollToTopBtn.style.opacity = "1";
+        scrollToTopBtn.style.visibility = "visible";
+    } else {
+        // 그렇지 않으면 버튼 숨김
+        scrollToTopBtn.style.opacity = "0";
+        scrollToTopBtn.style.visibility = "hidden";
+    }
+});
+
+// 버튼 클릭 이벤트
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth", // 부드럽게 스크롤
+    });
 });
